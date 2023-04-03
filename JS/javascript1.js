0b9d0c992c0706b3249198b84564d8dc97fe8e34
IvanIasnig <129066853+IvanIasnig@users.noreply.github.com>
2023-04-03 17:15:59 +0200

    Add files via upload

---
+++ JS/javascript1.js
@@ -0,0 +1,5 @@
+console.log("Messaggio di prova");
+
+document.getElementById("output").innerHTML = "Iniziamo la nostra pratica";
+
+alert("Messaggio di prova utilizzando il metodo alert");
---
+++ JS/javascript2.js
@@ -0,0 +1,16 @@
+var messaggio_di_prova_variabile =
+  "<div style='margin-left:20vw;'>pantera scritto con javascript</div>";
+
+var primo = "documento scritto con javascript";
+
+var messaggioDiProva = "Alert con costante";
+
+var MessaggioDiProvaCostante = "Secondo console.log";
+
+document.getElementById("pantera").innerHTML = primo;
+
+document.write(messaggio_di_prova_variabile);
+
+console.log(MessaggioDiProvaCostante);
+
+alert(messaggioDiProva);
